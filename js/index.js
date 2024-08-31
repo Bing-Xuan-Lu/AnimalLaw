2f864a8ae05aa76f52a014a3f164e88f0563ea91
Bing-Xuan-Lu <44064284+Bing-Xuan-Lu@users.noreply.github.com>
2024-09-01 03:53:16 +0800

    套件引用 最新消息 首頁調整

--- js/index.js
+++ js/index.js
@@ -1,10 +1,40 @@
 $(function () {
-    //alert(123);
-  $('#marquee').marquee({
-    speed: 8000,
+  //alert(123);
+  $("#marquee").marquee({
+    speed: 6000,
     gap: 60,
-    direction: 'left',
+    direction: "left",
     duplicated: true,
     delayBeforeStart: 0,
-});
+  });
+
+  let _queryTableConfig = {
+    "autoWidth": true,
+    "deferRender": false,
+    "info": false,
+    "lengthChange": true,
+    "ordering": true,
+    "paging": true,
+    "processing": true,
+    "scrollX": false,
+    "searching": false,
+    "serverSide": false,
+    "stateSave": false,
+    "destroy": true,
+    "displayStart": 0,
+    "orderCellsTop": false,
+    "orderClasses": false,
+    "orderMulti": false,
+    "pageLength": 10,
+    "retrieve": false,
+    "scrollCollapse": false,
+    "searchDelay": 0,
+    "stateDuration": 0,
+    "tabIndex": 0,
+    "language": {
+        "zeroRecords": "沒有符合的結果",
+        "url": "Scripts/datatables-zh-TW.js"
+    }
+};
+  $('#news').DataTable(_queryTableConfig);
 });
